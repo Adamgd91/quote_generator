e2fe35c68e527e728cff366a1a151db7abbb9824
Adam Davidson <agdavidson91@gmail.com>
2022-10-31 15:58:10 -0400

    added JS for API for quotes

---
+++ main.js
@@ -0,0 +1,11 @@
+// Get Quotes From API
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+  } catch (e) {
+    // Catching Error
+    alert("Error Error Error");
+  }
+}
